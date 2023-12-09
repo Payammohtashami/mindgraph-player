92b6c331c1fb4b08fb8af4ebd1c7acbde47cba15
Payam Mohtashami <p.mohtashami.1382.4@gmail.com>
2023-12-09 17:06:51 +0330

    ADD: add dependencies and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { JsPsych,  } from 'jspsych';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const jsPsych = new JsPsych();
+    console.log({jsPsych}, 'jsPsych');
+    
+    return (
+        <div className='container flex justify-center items-center'>
+            <h1 className='text-slate-800 font-bold text-4xl'>SYMMETRY</h1>
+        </div>
+    );
+};
 
 export default App;
